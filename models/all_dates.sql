0ebac67e0aec4b3157c44a8f94fe984a9e7b740c
Adrian Torres Roda <materializationsfundamentals@gmail.com>
2025-10-08 15:37:09 +0000

    ALL_DATES_MODIFIED

--- models/all_dates.sql
+++ models/all_dates.sql
@@ -4,7 +4,7 @@
 
 {{ dbt_utils.date_spine(
     datepart="day",
-    start_date="to_date('01/01/2020', 'mm/dd/yyyy')",
-    end_date="to_date('01/01/2021', 'mm/dd/yyyy')"
+    start_date = cast('01/01/2020' as date),
+    end_date = cast('01/01/2021', as date)
    )
 }}
