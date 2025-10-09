51a355048d9f39a49b2dd83db804a2c8e25612ef
Adrian Torres Roda <materializationsfundamentals@gmail.com>
2025-10-09 09:51:12 +0000

    ALL_DATES_REPAIRED

--- models/all_dates.sql
+++ models/all_dates.sql
@@ -2,9 +2,11 @@
     materialized="table"
 )}}
 
-{{ dbt_utils.date_spine(
-    datepart="day",
-    start_date = cast('01/01/2020' as date),
-    end_date = cast('01/01/2021' as date)
-   )
-}}
+SELECT
+    date_day
+FROM
+    ({{ dbt_utils.date_spine(
+        datepart="day",
+        start_date = "cast('2020-01-01' as date)", 
+        end_date = "cast('2021-01-01' as date)"
+    ) }})
