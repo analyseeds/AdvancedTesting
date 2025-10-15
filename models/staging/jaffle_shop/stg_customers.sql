4ce19255057ea9a36498b5c4faf82e2a36306ef6
Adrian Torres Roda <materializationsfundamentals@gmail.com>
2025-10-15 08:10:58 +0000

    SAVE_FAILURES

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,6 +1,23 @@
-select
-    id as customer_id,
-    first_name,
-    last_name
+with
 
-from {{ source('jaffle_shop','customers') }}
+customers as (
+
+    select
+        id as customer_id,
+        first_name,
+        last_name
+    from 
+        {{ source('jaffle_shop','customers') }}
+), 
+
+renamed as (
+
+    select
+        customer_id,
+        CONCAT(first_name,last_name) as full_name,
+        null as null_test
+    from 
+        customers
+)
+
+select * from renamed
